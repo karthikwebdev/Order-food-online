70a3841f345b61f7b2c1ddd4f8a116564a5768da
karthik enumarthi <kartheekenumarthi@gmail.com>
2020-05-26 14:12:06 +0530

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const router = express.Router();
+const monk = require('monk');
+const db = monk('localhost:27017/ofo');
+const users = db.get('users');
+const items = db.get('items');
+const orders = db.get('orders');
+
+/* GET home page. */
+router.get('/', (req, res) => {
+  users.find({"role":"1"},(err,docs)=>{
+    if(err || !docs){
+      console.log("there is an error: ",err,docs)
+    }else{
+      if(req.session && req.session.user){
+        res.render('index',{isloggedin:true,isOwner:req.session.user.role,names:docs})
+      }else{
+        res.render('index',{isloggedin:false,isOwner:false,names:docs});
+      }
+    }
+  })
+});
+
+/* GET profile page. */
+router.get('/profile',(req, res) => {
+  if(req.session && req.session.user){
+    res.render('profile',
+    {
+      name:req.session.user.name,
+      email:req.session.user.email,
+      phone:req.session.user.phone,
+      isOwner:req.session.user.role,
+      image:req.session.user.image
+    });
+  }else{
+    res.redirect('/')
+  }
+});
+
+/* GET items by id */
+router.get('/items/:id',(req,res) => {
+  if(req.session && req.session.user){
+    items.find({"owner":req.params.id},(err,docs)=>{
+      if(err || !docs){
+        console.log("something went wrong",err,docs)
+      }else{
+        res.render('items',{isOwner:req.session.user.role,docs,id:req.params.id})
+      }
+    })  
+  }else{
+  res.redirect('/login')
+  }
+})
+
+/* POST order from customer */
+router.post('/order',(req,res)=>{
+  if(req.session && req.session.user && req.body.items){
+    let items = JSON.parse(req.body.items) 
+    let uniqueItems = []
+    for(item of items){
+      if(!uniqueItems.includes(item)){
+        uniqueItems.push(item)
+      }
+    }
+    let final = []
+    for(uniqueitem of uniqueItems){
+      let count = 0
+      for(item of items){
+        if(item === uniqueitem) count++
+      }
+      final.push({name:uniqueitem,count:count})
+    }
+    orders.insert({items:final,customer:req.session.user,seller:req.body.id},(err,docs)=>{
+      if(err || !docs){
+        console.log(err,docs)
+      }else{
+        res.redirect('/')
+      }
+    })
+  }else{
+    res.redirect('/')
+  }
+})
+
+module.exports = router;
---
+++ routes/owner.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router = express.Router();
+const monk = require('monk');
+const db = monk('localhost:27017/ofo');
+const items = db.get('items');
+const orders = db.get('orders');
+
+/* GET Add item page. */
+router.get('/additem',(req, res) => {
+    if(req.session && req.session.user && (req.session.user.role == 1)){
+        res.render('additem',{success:false,item:false});
+    }else{
+      res.redirect('/')
+    }
+  });
+  
+/* POST items to db */
+router.post('/additem',(req,res) => {
+    if(req.session && req.session.user && (req.session.user.role == 1)){
+        let details = {
+            item:req.body.name,
+            price:req.body.price,
+            owner:req.session.user._id
+        }
+        items.insert(details,(err,docs)=>{
+            if(err){
+                console.log(err)
+            }else{
+                res.render('additem',{success:true,item:docs.item})
+            }
+        })
+    }
+})
+
+/* GET orders page. */
+router.get('/orders',(req, res) => {
+    if(req.session && req.session.user && (req.session.user.role == 1)){
+        orders.find({"seller":req.session.user._id},(err,docs)=>{
+            if(err || !docs){
+                console.log(err)
+            }else{
+                res.render('orders',{docs:docs});
+            }
+        })
+      }else{
+        res.redirect('/')
+      }
+  });
+
+module.exports = router;
